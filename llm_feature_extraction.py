3ddc016eabac05fb195506e070fee2181eb95b06
Héctor Garrido <h.f.g.r.1996@gmail.com>
2024-09-17 19:54:28 -0600

    mod llm_feature_extraction: update api and model
    change prompts, update default variables, move to gpt-4o-mini, refactor code for new api version, comment out old code

--- llm_feature_extraction.py
+++ llm_feature_extraction.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import openai
 from dotenv import load_dotenv
+import json
 
 ################################################################################
 # load env
@@ -10,9 +11,9 @@ load_dotenv()
 
 openai.organization = os.getenv("OPENAI_ORG_ID")
 openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.Model.list()
 
 ################################################################################
+# def funcitions
 
 def output_to_dict(output):
 
@@ -34,10 +35,18 @@ def output_to_dict(output):
         
         return out_dict
     
+def output_to_dict_1(output):
+    output = output.split("```")[1]
+    output = output.replace('\n','').replace('json','')
+    output = json.loads(
+        output    
+    )
+    return output
+
 ################################################################################
 # def filenames
 
-custom_filename = input("¿Cuentas con un archivo csv particular? y/n: ")
+custom_filename = input("¿Cuentas con un archivo csv con un nombre distinto al default del programa anterior? y/n: ")
 if custom_filename == "y":
     INPUT_FILE_PATH = input("Introduce el nombre del archivo csv: ")
 elif custom_filename == "n":
@@ -51,16 +60,18 @@ OUTPUT_FILE_PATH = 'llm_' + INPUT_FILE_PATH
 ################################################################################
 # prepare prompt
 
-vars_prompt = "fecha del evento, edad de la víctima, transporte del victimario, transporte de la víctima y ubicación del accidente"
+vars_prompt = "fecha del evento, nombre de la víctima, género de la víctima, edad de la víctima, nombre del victimario, edad del victimario, género del victimario, transporte del victimario, medio de transporte de la víctima, si la víctima salió volando,  si la víctima tomó alcohol, si el victimario tomó alcohol, ubicación del accidente, municipio del accidente, localidad del accidente, en cuántos días murió la víctima, cómo murió la víctima y si el victimario fue detenido"
+# vars_prompt = "fecha del evento, ubicación del evento, municipio, estado, dirección del evento, identidad del victimario, si hubo muertos, si hubo homicidios, tipo de crimen y tipo de objeto o bien robado"
+# vars_prompt = "fecha del evento, edad de la víctima, transporte del victimario, transporte de la víctima y ubicación del accidente"
 custom_prompt = input("¿Cuentas con una lista de variables personalizada? y/n: ")
-if custom_filename == "y":
+if custom_prompt == "y":
     vars_prompt = input("Introduce tu lista personalizada: ")
-elif custom_filename != "n":
+elif custom_prompt != "n":
     print("Comando desconocido. Interrumpiendo programa.")
     exit(1)
 
 #prompt_0 = f"Extraer {vars_prompt}:\n "
-prompt_0 = f"Extract {vars_prompt}:\n "
+prompt_0 = f"Extrae en formato json las variables {vars_prompt}:\n "
 
 ################################################################################
 # read dataset
@@ -84,29 +95,51 @@ for var in list_vars_prompt:
 # perform requests
 
 for i in df[ 
-    ~df.texto_0.str.contains('weve detected|requested url|requested resource|access the url',case=False) & 
-    ~df.texto_0.str.contains('error 404',case=False) & 
-    df.texto.notnull()
-    ].head(40).index:
+    (~df.texto_0.str.contains('weve detected|requested url|requested resource|access the url',case=False)) & 
+    (~df.texto_0.str.contains('error 404',case=False)) & 
+    (df['URL noticia'].str.slice(-4)!='.pdf') & 
+    (df.texto.notnull())
+    ].index:
 
     # set up the prompt and model parameters
+    MODEL = "gpt-4o-mini"
+    LLM_MODEL_TOKEN_LIMIT = 128000
+    # rule assuming conservatively that tokens (words) have 3 characters on average
+    LLM_MODEL_CHAR_LIMIT = 3*LLM_MODEL_TOKEN_LIMIT
+
     prompt = prompt_0
     prompt += df.loc[i, 'texto']
     df.loc[i, 'prompt_length'] = len(prompt)
-    prompt = prompt[:4097]
+    prompt = prompt[:LLM_MODEL_CHAR_LIMIT]
 
-    model = "text-davinci-003"    
     try:
         # make the request to the OpenAI API
-        response = openai.Completion.create(
-          engine=model,
-          prompt=prompt,
-          max_tokens=1900,
-          n=1,
-          stop=None,
-          temperature=0,
+        # response = openai.ChatCompletion.create(
+        response = openai.chat.completions.create(
+          model=MODEL,
+          messages=[{'role':'user','content':prompt}]
+        #   prompt=prompt,
+        #   max_tokens=16000,
+        #   max_tokens=1900,
+        #   n=1,
+        #   stop=None,
+        #   temperature=0,
         )
-        output = response.choices[0].text
+        # response = openai.chat.completions.create(
+        #   engine=model,
+        #   prompt=prompt,
+        #   max_tokens=16000,
+        # #   max_tokens=1900,
+        #   n=1,
+        #   stop=None,
+        #   temperature=0,
+        # )
+        # print(response)
+        # print(dir(response))
+        output = response.choices[0].message.content
+        print(df['URL noticia'][i])
+        # print(df['texto'][i])
+        # print(output)
 
     except Exception as error:
         print('Caught this error: ' + repr(error))
@@ -114,13 +147,22 @@ for i in df[
     
     df.loc[i, 'llm_output'] = output
     
-    out_dict = output_to_dict(output)    
-    if out_dict!=None:    
-        for key, value in out_dict.items():
-            if key in df.columns:
-                df.loc[i, key] = value
+    # out_dict = output_to_dict(output)    
+    # if out_dict!=None:    
+    #     for key, value in out_dict.items():
+    #         if key in df.columns:
+    #             df.loc[i, key] = value
+    try:
+        out_dict = output_to_dict_1(output)
+        out_list = list(out_dict.values())    
+        for j in range(len(out_list)):
+            df.iloc[i, -(1+j)] = out_list[-(1+j)]
+
+    except Exception as error:
+        print('Caught this error: ' + repr(error))
             
     print(i)
+    print(output)
 
 ################################################################################
 # export
